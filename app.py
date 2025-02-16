6da4e320edb1e5b8cdf11a2bc2f15829453a7d02
Hakeem Manwahab <hakeemmwh2@gmail.com>
2025-02-16 23:19:46 +0700

    gallery html

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-from flask import Flask
-from flask import render_template
-app = Flask(__name__)  
-@app.route("/")
+# app.py
+from flask import Flask, request, render_template, redirect, url_for
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
 def index():
-    return "1st Commit"
+    images = os.listdir(app.config['UPLOAD_FOLDER'])
+    return render_template('gallery.html', images=images)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_image():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+            return redirect(url_for('index'))
+    return render_template('upload.html')
+
 if __name__ == "__main__":
-    app.run
+    app.run(debug=True)
