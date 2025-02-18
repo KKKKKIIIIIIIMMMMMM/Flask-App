40319ee3cc36686bf2b895801089a5639a354673
Hakeem Manwahab <hakeemmwh2@gmail.com>
2025-02-18 13:22:27 +0700

    Fix login bug

---
+++ KIM-Gallery/kim_gallery/main/forms.py
@@ -0,0 +1,27 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, ValidationError
+from kim_gallery.models import User
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=64)])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(max=120)])
+    about_me = TextAreaField('About me', validators=[Length(max=500)])
+    submit = SubmitField('Save Changes')
+
+    def __init__(self, original_username, original_email, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+        self.original_email = original_email
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        if email.data != self.original_email:
+            user = User.query.filter_by(email=email.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different email address.') 
--- KIM-Gallery/kim_gallery/main/routes.py
+++ KIM-Gallery/kim_gallery/main/routes.py
@@ -1,7 +1,9 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_required
 from kim_gallery.main import bp
-from kim_gallery.models import Image, Tag
+from kim_gallery.models import Image, Tag, User
+from kim_gallery import db
+from kim_gallery.main.forms import EditProfileForm
 
 @bp.route('/')
 @bp.route('/index')
@@ -10,6 +12,32 @@ def index():
     images = Image.query.order_by(Image.upload_date.desc()).paginate(
         page=page, per_page=12, error_out=False)
     return render_template('main/index.html', title='Home', images=images)
+
+@bp.route('/profile/<username>')
+@login_required
+def profile(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    page = request.args.get('page', 1, type=int)
+    images = user.images.order_by(Image.upload_date.desc()).paginate(
+        page=page, per_page=12, error_out=False)
+    return render_template('main/profile.html', user=user, images=images)
+
+@bp.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(current_user.username, current_user.email)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.', 'success')
+        return redirect(url_for('main.profile', username=current_user.username))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+        form.about_me.data = current_user.about_me
+    return render_template('main/edit_profile.html', title='Edit Profile', form=form)
 
 @bp.route('/about')
 def about():
